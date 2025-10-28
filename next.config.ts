bd461cb447029d4135b02eec97edcaaaaeae1ba4
bommalamrudhulraj-ship-it <bommalamrudhulraj@gmail.com>
2025-10-27 21:57:09 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -9,11 +9,19 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
-        hostname: "m.media-amazon.com", 
+        hostname: "m.media-amazon.com",
       },
       {
         protocol: "https",
-        hostname: "image.tmdb.org", 
+        hostname: "image.tmdb.org",
+      },
+      {
+        protocol: "https",
+        hostname: "encrypted-tbn0.gstatic.com", // fallback images Google uses
+      },
+      {
+        protocol: "https",
+        hostname: "static1.colliderimages.com", // Tron placeholder sometimes uses this
       },
     ],
   },
